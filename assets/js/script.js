58ec7d6e3ac1a260172abdb78abe21b1de7c3fec
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-07 16:00:22 +0200

    add images in DOM

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const slides = [
+    {
+        pic : './assets/img/01.webp'
+    },
+    {
+        pic : './assets/img/02.webp'
+    },
+    {
+        pic : './assets/img/03.webp'
+    },
+    {
+        pic : './assets/img/04.webp'
+    },
+    {
+        pic : './assets/img/05.webp'
+    },
+]
+
+let sliderImages = document.querySelector('.images')
+
+
+slides.forEach(myFnc);
+
+function myFnc(slide) {
+
+    let slideDom = `<img src="${slide.pic}" class="my_w">` 
+    sliderImages.insertAdjacentHTML('beforeend', slideDom)   
+    
+}
+
+
